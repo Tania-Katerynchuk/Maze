4a04a3921e9a32d07aad69904db532d39c4c56da
Tania Katerynchuk <3598tania@gmail.com>
2020-09-03 18:09:08 +0300

    v1

---
+++ Assets/Scripts/MazeSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MazeSpawner : MonoBehaviour
+{
+    public Cell CellPrefab;
+    private Vector3 CellSize = new Vector3(10,0,10);
+    public GameObject Finish;
+
+    public Maze maze;
+
+    private void Start()
+    {
+        MazeGenerator generator = new MazeGenerator();
+        maze = generator.GenerateMaze();
+
+        for (int x = 0; x < maze.cells.GetLength(0); x++)
+        {
+            for (int y = 0; y < maze.cells.GetLength(1); y++)
+            {
+                Cell c = Instantiate(CellPrefab, new Vector3(x * CellSize.x, y * CellSize.y, y * CellSize.z), Quaternion.identity);
+
+                c.WallLeft.SetActive(maze.cells[x, y].WallLeft);
+                c.WallBottom.SetActive(maze.cells[x, y].WallBottom);
+            }
+        }
+        Finish.transform.position  = new Vector3( 10*maze.finishPosition.x + 5, (float)1.2, 10*maze.finishPosition.y + 5);
+    }
+}
